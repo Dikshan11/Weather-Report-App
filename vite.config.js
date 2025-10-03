96a4752a318852ff386d56a4edfeab808690d651
Dikshan V V <vvdikshan1104@gmail.com>
2025-10-03 23:49:56 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base:'/Weather-Report-App/'
-})
+  base: "/weather-report-app",
+});
